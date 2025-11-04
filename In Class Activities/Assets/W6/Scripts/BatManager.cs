85641ccad1a6b3456cd99bf7e844c42c80d38cf8
PeterY4ng <ziyuey15@uci.edu>
2025-11-03 21:13:20 -0800

    1

--- In Class Activities/Assets/W6/Scripts/BatManager.cs
+++ In Class Activities/Assets/W6/Scripts/BatManager.cs
@@ -12,14 +12,14 @@ public class BatManager : MonoBehaviour
     // STEP 1 -----------------------------------------------------------------
     // Add a member variable named "_bats" that's an array of BatW6 Components.
     // In the Inspector, add ALL of the bats in the Scene.
-    
+    [SerializeField] private BatW6[] _bats;
     // STEP 1 -----------------------------------------------------------------
 
     // STEP 3 -----------------------------------------------------------------
     // Add a member variable named "_messages" that's an array of strings.
     // In the Inspector, add at least a few different messages for the bats to
     //      say when they reach the player.
-    
+    [SerializeField] private string[] _messages;
     // STEP 3 -----------------------------------------------------------------
 
     [SerializeField] private float[] _newTextTimers;
@@ -38,7 +38,7 @@ public class BatManager : MonoBehaviour
         // That means the bat at _bats[0] has a timer at _newTextTimers[0],
         //      the bat at _bats[1] has a timer at _newTextTimers[1],
         //      and so on.
-        // _newTextTimers = new [_bats.Length];
+        _newTextTimers = new float [_bats.Length];
         // STEP 6 -------------------------------------------------------------
     }
 
@@ -49,6 +49,10 @@ public class BatManager : MonoBehaviour
         // Loop through all of the entries in _newTextTimers, and increase each
         //      timer's value by the amount of time that passed this frame.
         
+        for (int i = 0; i < _newTextTimers.Length; i++)
+        {
+            _newTextTimers[i] += Time.deltaTime;
+        }
 
         // STEP 7 -------------------------------------------------------------
 
@@ -63,12 +67,11 @@ public class BatManager : MonoBehaviour
         // You will need to check the Vector3 documentation to find a method
         //      to help you with that distance check :)
         // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.html
-        //
         // STEP 4
         // Also inside this for loop, if the distance between the bat and the
         //      player is less than _overlapDistance, call CreateReactions()
         //      and pass the bat in as an argument.
-        
+
 
 
         // STEP 2 -------------------------------------------------------------
@@ -81,18 +84,19 @@ public class BatManager : MonoBehaviour
         // STEP 5 -------------------------------------------------------------
         // The Random.Range() method can be used to give us a random number
         //      between two values.
+        int randomIndex = Random.Range(0, _messages.Length);
         // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Random.Range.html
         // If you use floats as the arguments, it will return a float.
         // If you use ints as the arugments, it will return an int.
-        //
         // Use Random.Range() to pick a RANDOM INDEX in the _messages array
-        //      and call SpawnReactionUI() with the string at that index.
+        //      and call SpawnReactionUI() with the string at that index
+        string randomMessage = _messages[randomIndex];
         // This should choose a random message to spawn every time one of the
         //      bats reaches the player.
         //
         // The first argument to SpawnReactionUI is same bat in the parameters
         //      of CreateReactions.
-        
+
         // STEP 5 -------------------------------------------------------------
     }
 
@@ -107,8 +111,8 @@ public class BatManager : MonoBehaviour
         // /* starts the comments, and */ ends it.
         // Simply uncomment the below lines by removing the /* and */ to finish.
 
-        /*
-        int index = System.Array.IndexOf(_bats, bat);
+        
+        [index] = System.Array.IndexOf(_bats, bat);
         
         GridLayoutGroup layout = bat.GetComponentInChildren<GridLayoutGroup>();
         if (layout != null && _newTextTimers[index] >= _timeBetweenNewMessages)
@@ -117,8 +121,8 @@ public class BatManager : MonoBehaviour
             TMP_Text textObj = Instantiate(_reactionUiPrefab, layout.transform);
             textObj.text = message;
         }
-        */
-
+        
+       
         // STEP 8 -------------------------------------------------------------
     }
 }
